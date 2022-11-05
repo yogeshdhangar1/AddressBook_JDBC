7eeafd5393ca47a0ac53b874c0ae318fab66a667
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-11-05 10:20:53 -0700

    Added: Update Data By Using Prepared Statement

--- src/main/java/AddressBook_JDBC.java
+++ src/main/java/AddressBook_JDBC.java
@@ -1,17 +1,20 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
+
 public class AddressBook_JDBC {
-    public static void main(String[] args) {
-        System.out.println("Welcome To Java DataBase Connectivity");
-        try {
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_servicedb","root","mysql@1234");
-            Statement stm = con.createStatement();
-           stm.execute("update emp_service set salary = 300000.00 where name = 'terisa'");
-            stm.execute("select * from emp_service");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    public static void preparedStatement(String name , int basePay) throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_servicedb","root","mysql@1234");
+        String q="update  emp_service set salary=?  where name=?";
+        PreparedStatement pstm = con.prepareStatement(q);
+         pstm.setLong(1,basePay);
+         pstm.setString(2,name);
+       pstm.executeUpdate();
+       boolean result = true;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        preparedStatement("Terisa",300000);
     }
 }
