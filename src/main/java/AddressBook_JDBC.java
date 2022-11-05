6d8fb439a88dbfcee54999a172a5742cbb72c2c4
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-11-05 10:53:25 -0700

    Added: Test Update Query By Using Prepared Statement

--- src/main/java/AddressBook_JDBC.java
+++ src/main/java/AddressBook_JDBC.java
@@ -4,17 +4,17 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class AddressBook_JDBC {
-    public static void preparedStatement(String name , int basePay) throws SQLException {
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_servicedb","root","mysql@1234");
-        String q="update  emp_service set salary=?  where name=?";
-        PreparedStatement pstm = con.prepareStatement(q);
-         pstm.setLong(1,basePay);
-         pstm.setString(2,name);
-       pstm.executeUpdate();
-       boolean result = true;
+    public static boolean CreateConnection(String q) {
+        return true;
     }
+        public static boolean preparedStatement (String name ,int basePay) throws SQLException {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_servicedb", "root", "mysql@1234");
+            String q = "update  emp_service set salary=?  where name=?";
+            PreparedStatement pstm = con.prepareStatement(q);
+            pstm.setLong(1, basePay);
+            pstm.setString(2, name);
+            pstm.executeUpdate();
+            return true;
+        }
 
-    public static void main(String[] args) throws SQLException {
-        preparedStatement("Terisa",300000);
-    }
 }
